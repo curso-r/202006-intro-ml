4dcd1e8c2779cfff89dc5b0e020b8c2c67a8f857
Athos Damiani <athos.damiani@gmail.com>
2020-07-02 22:05:36 -0300

    pós aula 4

--- exemplos/04-hiperparametros.R
+++ exemplos/04-hiperparametros.R
@@ -1,5 +1,5 @@
 # Pacotes ------------------------------------------------------------------
-
+# grid search e random search
 library(ggplot2)
 library(tidymodels)
 
@@ -17,9 +17,9 @@ diamonds_test <- testing(diamonds_initial_split)
 # passando parâmetros específicos das "engines"
 diamonds_model <- decision_tree(
   cost_complexity = 0,
-  min_n = tune(), 
+  min_n = tune(),
   tree_depth = tune()
-) %>% 
+) %>%
   set_engine("rpart") %>%
   set_mode("regression")
 
@@ -29,19 +29,19 @@ diamonds_resamples <- vfold_cv(diamonds_train, v = 5)
 # grid na mao -------------------------------------------------------------
 
 # ASSIM: parameters() + update() + cost_complexity() + grid_regular()
-hiperparams <- parameters(diamonds_model) %>% 
+hiperparams <- parameters(diamonds_model) %>%
   update(
-    min_n = min_n(range = c(2L, 10L)),
-    tree_depth = tree_depth(c(2L, 10L))
+    min_n = min_n(range = c(2L, 12L)),
+    tree_depth = tree_depth(c(2L, 12L))
   ) %>%
   grid_regular(levels = 10)
 
 # ASSIM: parameters() + update() + cost_complexity() + grid_random()
-hiperparams <- parameters(diamonds_model) %>% 
+hiperparams <- parameters(diamonds_model) %>%
   update(
     min_n = min_n(range = c(2L, 5L))
   ) %>%
-  grid_random(size = 5)
+  grid_random(size = 100)
 
 # OU ASSIM: expand.grid()
 hiperparams <- expand.grid(
@@ -49,10 +49,12 @@ hiperparams <- expand.grid(
   tree_depth = c(2, 6, 10, 14)
 )
 
+plot(min_n~tree_depth, data = hiperparams)
+
 # tunagem de hiperparametros ----------------------------------------------
 # com tune_grid()
 diamonds_tune_grid <- tune_grid(
-  price ~ x, 
+  price ~ x,
   diamonds_model,
   resamples = diamonds_resamples,
   grid = hiperparams,
@@ -67,7 +69,7 @@ diamonds_best_hiperparams <- select_best(diamonds_tune_grid, "rmse")
 diamonds_model <- diamonds_model %>% finalize_model(diamonds_best_hiperparams)
 
 # ou com set_args()
-diamonds_model <- diamonds_model %>% 
+diamonds_model <- diamonds_model %>%
   set_args(
     cost_complexity = diamonds_best_hiperparams$cost_complexity,
     min_n = 5
